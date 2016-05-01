cd3076688776f7bd29a73926e7f141b63430fbc4
Lukasz Bajor <lukaszic@o2.pl>
2016-05-02 01:11:05 +0200

    Failing feature test to price simple basket with 3 items - no offers / Passingunit test to price 1 item basket

--- src/main/scala/bajor/lukasz/basketpricer/exhaustive/Basket.scala
+++ src/main/scala/bajor/lukasz/basketpricer/exhaustive/Basket.scala
@@ -3,6 +3,6 @@ package bajor.lukasz.basketpricer.exhaustive
 /**
   * Created by lbajor on 2016-04-30.
   */
-case class Basket() {
-  def this(items: Item*) = this()
+case class Basket(val items: List[Item]) {
+  def this(items: Item*) = this(items.toList)
 }
--- src/main/scala/bajor/lukasz/basketpricer/exhaustive/BasketPricer.scala
+++ src/main/scala/bajor/lukasz/basketpricer/exhaustive/BasketPricer.scala
@@ -4,5 +4,10 @@ package bajor.lukasz.basketpricer.exhaustive
   * Created by lbajor on 2016-04-30.
   */
 class BasketPricer() {
-  def price(basket: Basket): BigDecimal = BigDecimal(0)
+  def price(basket: Basket) = {
+    if (basket.items.isEmpty)
+      BigDecimal(0)
+    else
+      basket.items.head.price
+  }
 }
