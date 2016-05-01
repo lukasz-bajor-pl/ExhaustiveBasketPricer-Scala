4f48cab1b057572648c83b8f4d4815fda85be71d
Lukasz Bajor <lukaszic@o2.pl>
2016-05-02 01:00:10 +0200

    Passing simple test pricing empty basket as 0 - no offers

--- src/main/scala/bajor/lukasz/basketpricer/exhaustive/BasketPricer.scala
+++ src/main/scala/bajor/lukasz/basketpricer/exhaustive/BasketPricer.scala
@@ -4,6 +4,6 @@ package bajor.lukasz.basketpricer.exhaustive
   * Created by lbajor on 2016-04-30.
   */
 class BasketPricer() {
-  def price(basket: Basket): BigDecimal = null
+  def price(basket: Basket): BigDecimal = BigDecimal(0)
 
 }
